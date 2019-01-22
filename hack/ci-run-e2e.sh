fc2df954102c1596282b87640d071e006ccae48a
Tomas Nozicka <tnozicka@gmail.com>
2019-01-22 12:13:19 +0100

    Sync

--- hack/ci-run-e2e.sh
+++ hack/ci-run-e2e.sh
@@ -1,39 +1,17 @@
 #!/usr/bin/env bash
 set -eEuxo pipefail
-#trap "sleep infinity" ERR
-
-env
-
-oc create route edge test --service a --port 8080 && oc get route test -o yaml
-
-sleep infinity
-
-#ngrok=$( which ngrok || true)
-#if [ -z ${ngrok} ]; then
-#    bin=$( mktemp -d)
-#    curl -L https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip | bsdtar -xvf- -C ${bin} && chmod +x ${bin}/ngrok
-#    ngrok=${bin}/ngrok
-#fi
-#
-#logfile=$( mktemp )
-#${ngrok} http -log stdout -log-level debug -log-format json 5000 > ${logfile} &
-#
-#export TEST_DOMAIN=$( tail -n +0 -f ${logfile} | grep --text -m 1 'Hostname' | sed -r 's/.*Hostname:([^] ]*).*/\1/') || [[ $? == 141 ]]
-#echo "TEST_DOMAIN: '${TEST_DOMAIN}'"
-
-export TEST_DOMAIN=$( cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1 || [[ $? == 141 ]] ).serveo.net
-source "$(dirname "${BASH_SOURCE[0]}")/lib/nss_wrapper.sh"
-nss_wrapper $( mktemp -d )
-ssh -o StrictHostKeyChecking=no -R ${TEST_DOMAIN}:80:localhost:5000 serveo.net &
-
-oc port-forward -n default dc/router 5000:80 &
-
-oc get projects
+trap "sleep infinity" ERR
 
 # Deploy
 project=acme-$( cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1 || [[ $? == 141 ]] )
 oc new-project ${project}
 oc adm pod-network make-projects-global ${project} || true
+
+#FIXME:remove test
+curl $( oc get route test -o go-template --template={{.spec.host}} )
+
+export TEST_DOMAIN=""
+# Likely not needed given we get unique domains in each namespace
 export DELETE_ACCOUNT_BETWEEN_STEPS_IN_NAMESPACE=${project}
 
 oc create -fdeploy/letsencrypt-staging/cluster-wide/imagestream.yaml
